85320cd51139526823f14446217987b4f4ef0b41
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2023-06-05 23:00:59 +0200

    test: don't ignore whitespace when diffing documents

--- src/tests/FlashOWare.Tool.Core.Tests/Assertions/RoslynAssert.cs
+++ src/tests/FlashOWare.Tool.Core.Tests/Assertions/RoslynAssert.cs
@@ -51,7 +51,7 @@ internal static class RoslynAssert
 
                     """);
 
-                var diff = InlineDiffBuilder.Diff(expectedText, actualText, true, false, null);
+                var diff = InlineDiffBuilder.Diff(expectedText, actualText, false, false, null);
 
                 foreach (var line in diff.Lines)
                 {
