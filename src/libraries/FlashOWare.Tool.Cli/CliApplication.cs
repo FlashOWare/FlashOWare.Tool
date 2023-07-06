e9f42ed53868d22d1e7e45e57f2ad648aa7610df
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2023-07-06 17:53:42 +0200

    feat: use System.CommandLine (#40)
    Co-authored-by: Eva Ditzelmüller <87754804+EvaDitzelmueller@users.noreply.github.com>

--- src/libraries/FlashOWare.Tool.Cli/CliApplication.cs
+++ src/libraries/FlashOWare.Tool.Cli/CliApplication.cs
@@ -1,118 +1,57 @@
-using FlashOWare.Tool.Cli.CodeAnalysis;
-using FlashOWare.Tool.Cli.Diagnostics;
-using FlashOWare.Tool.Core.UsingDirectives;
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.MSBuild;
 using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace FlashOWare.Tool.Cli;
 
-public static class CliApplication
+public static partial class CliApplication
 {
     public static async Task<int> RunAsync(string[] args)
     {
-        ConsoleColor color = Console.ForegroundColor;
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("2code ^ !2code...that is the question!");
-        Console.ForegroundColor = color;
-
-        using var cancellation = new CancellationTokenSource();
-        Action<PosixSignalContext> handler = (PosixSignalContext context) =>
-        {
-            Debug.Assert(context.Signal == PosixSignal.SIGINT || context.Signal == PosixSignal.SIGQUIT || context.Signal == PosixSignal.SIGTERM);
-
-            context.Cancel = true;
-            cancellation.Cancel();
-        };
-        using var sigInt = PosixSignalRegistration.Create(PosixSignal.SIGINT, handler);
-        using var sigQuit = PosixSignalRegistration.Create(PosixSignal.SIGQUIT, handler);
-        using var sigTerm = PosixSignalRegistration.Create(PosixSignal.SIGTERM, handler);
-
         var msBuild = MSBuildLocator.RegisterDefaults();
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"MSBuild ({msBuild.DiscoveryType}): {msBuild.Name} {msBuild.Version}");
-        Console.ForegroundColor = color;
 
         var properties = ImmutableDictionary<string, string>.Empty.Add("Configuration", "Release");
         using var workspace = MSBuildWorkspace.Create(properties);
         workspace.WorkspaceFailed += OnWorkspaceFailed;
 
-        if (args is ["using", "count", string path1])
-        {
-            await CountUsingsAsync(workspace, path1, cancellation.Token);
-        }
-        else if (args is ["using", "globalize", string localUsing, string path2])
-        {
-            await GlobalizeUsingsAsync(workspace, path2, localUsing, cancellation.Token);
-        }
-        else
-        {
-            Console.WriteLine("""
-                Commands:
-                  flashoware using count <PROJECT>
-                  flashoware using globalize <USING> <PROJECT>
-                """);
-        }
+        var aboutOption = new Option<bool>(new string[] { "--about", "-a", "-!" }, "Show application information");
+        var infoOption = new Option<bool>(new string[] { "--info", "-i", "-#" }, "Show environment information");
 
+        var rootCommand = new RootCommand("A .NET tool that facilitates development workflows.")
+        {
+            Name = "flashoware",
+        };
+        rootCommand.Add(aboutOption);
+        rootCommand.Add(infoOption);
+
+        rootCommand.SetHandler((InvocationContext context) =>
+        {
+            if (context.BindingContext.ParseResult.GetValueForOption(aboutOption))
+            {
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("2code ^ !2code...that is the question!");
+                Console.ForegroundColor = color;
+            }
+
+            if (context.BindingContext.ParseResult.GetValueForOption(infoOption))
+            {
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"MSBuild ({msBuild.DiscoveryType}): {msBuild.Name} {msBuild.Version}");
+                Console.ForegroundColor = color;
+            }
+        });
+
+        AddUsingCommand(rootCommand, workspace);
+
+        int exitCode = await rootCommand.InvokeAsync(args);
         workspace.WorkspaceFailed -= OnWorkspaceFailed;
-        return ExitCodes.Success;
+        return exitCode;
     }
 
     private static void OnWorkspaceFailed(object? sender, WorkspaceDiagnosticEventArgs e)
     {
         Console.WriteLine(e.Diagnostic.ToString());
-    }
-
-    private static async Task CountUsingsAsync(MSBuildWorkspace workspace, string projectFilePath, CancellationToken cancellationToken)
-    {
-        try
-        {
-            Project project = await workspace.OpenProjectAsync(projectFilePath, null, cancellationToken);
-
-            var result = await UsingCounter.CountAsync(project, cancellationToken);
-            Console.WriteLine($"{nameof(Project)}: {result.ProjectName}");
-            foreach (var usingDirective in result.Usings)
-            {
-                Console.WriteLine($"  {usingDirective.Name}: {usingDirective.Occurrences}");
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            await Console.Out.WriteLineAsync("Operation canceled.");
-        }
-    }
-
-    private static async Task GlobalizeUsingsAsync(MSBuildWorkspace workspace, string projectFilePath, string localUsing, CancellationToken cancellationToken)
-    {
-        try
-        {
-            Project project = await workspace.OpenProjectAsync(projectFilePath, null, cancellationToken);
-
-            workspace.ThrowIfCannotApplyChanges(ApplyChangesKind.AddDocument, ApplyChangesKind.ChangeDocument);
-            var result = await UsingGlobalizer.GlobalizeAsync(project, localUsing, cancellationToken);
-            workspace.ApplyChanges(result.Project.Solution);
-
-            Console.WriteLine($"{nameof(Project)}: {result.Project.Name}");
-
-            if (result.Using.Occurrences == 0)
-            {
-                Console.WriteLine($"""No occurrences of Using Directive "{localUsing}" were globalized.""");
-            }
-            else if (result.Using.Occurrences == 1)
-            {
-                Console.WriteLine($"""1 occurrence of Using Directive "{localUsing}" was globalized to "{result.TargetDocument}".""");
-            }
-            else
-            {
-                Console.WriteLine($"""{result.Using.Occurrences} occurrences of Using Directive "{localUsing}" were globalized to "{result.TargetDocument}".""");
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            await Console.Out.WriteLineAsync("Operation canceled.");
-        }
     }
 }
---
+++ src/libraries/FlashOWare.Tool.Cli/Properties/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Microsoft.CodeAnalysis.MSBuild;
+global using System.CommandLine;
+global using System.CommandLine.Invocation;
