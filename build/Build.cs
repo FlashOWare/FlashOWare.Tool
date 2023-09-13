90e384038d67d165d10c8cebbacb801038858169
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2023-09-13 21:17:49 +0200

    ci: add macOS and Ubuntu
    Co-Authored-By: Eva Ditzelmüller <87754804+EvaDitzelmueller@users.noreply.github.com>

--- build/Build.cs
+++ build/Build.cs
@@ -1,37 +1,31 @@
 using System;
 using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
 using Nuke.Common.Tools.DotNet;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions(
-    "publish",
-        GitHubActionsImage.WindowsServer2022,
-        OnPushBranches = new[] { "publish" },
-        InvokedTargets = new[] { nameof(Publish) },
-        ImportSecrets = new[] { nameof(NuGetApiKey) })]
-[GitHubActions(
     "ci",
+        GitHubActionsImage.MacOs12,
+        GitHubActionsImage.Ubuntu2204,
         GitHubActionsImage.WindowsServer2022,
         OnPushBranches = new[] { "main" },
         OnPullRequestBranches = new[] { "main" },
-        //FetchDepth = 1,
+        FetchDepth = 1,
         InvokedTargets = new[] { nameof(Test), nameof(Pack) })]
-// [GitHubActions(
-//     "ci",
-//         GitHubActionsImage.Ubuntu2204,
-//         On = new[] { GitHubActionsTrigger.Push, GitHubActionsTrigger.PullRequest})]
+[GitHubActions(
+    "publish",
+        GitHubActionsImage.Ubuntu2204,
+        OnPushBranches = new[] { "publish" },
+        FetchDepth = 1,
+        InvokedTargets = new[] { nameof(Publish) },
+        ImportSecrets = new[] { nameof(NuGetApiKey) })]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -59,44 +53,31 @@ class Build : NukeBuild
             ArtifactsDirectory.DeleteDirectory();
         });
 
-    // nuke --solution1 <value>
-    // env var: SOLUTION1 / NUKE_SOLUTION1
-    // parameters.json
-    [Parameter] readonly string Solution1 = "default.sln";
+    [Solution] readonly Solution Solution;
 
     [Parameter] readonly string Version;
 
-    [Solution] readonly Solution Solution;
+    [Parameter] readonly string NuGetSource;
 
     [Parameter] [Secret] readonly string NuGetApiKey;
-
-    [Parameter] readonly string NuGetSource;
 
     Target Restore => _ => _
         .Executes(() =>
         {
-            // dotnet restore <solution>
-            DotNet($"restore {Solution.Path}");
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
-            // dotnet build <solution> --configuration Debug/Release
             DotNetBuild(_ => _
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
                 .SetNoRestore(FinishedTargets.Contains(Restore)));
-
-            // nuke compile --skip (skip all dependencies)
-            // nuke publish --skip restore (execute compile and publish)
-            // nuke publish compile --skip (execute compile and publish)
         });
 
-    //      v- root directory
-    // <parent>/artifacts/test-results   (unix)
-    // <parent>\artifacts\test-results
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
     AbsolutePath TestResultsDirectory => ArtifactsDirectory / "test-results";
     AbsolutePath PackageDirectory => ArtifactsDirectory / "package";
@@ -105,7 +86,6 @@ class Build : NukeBuild
         .DependsOn(Compile)
         .Executes(() =>
         {
-            //dotnet test sln --no-build --results-directory <path>
            DotNetTest(_ => _
                .SetProjectFile(Solution)
                .SetConfiguration(Configuration)
