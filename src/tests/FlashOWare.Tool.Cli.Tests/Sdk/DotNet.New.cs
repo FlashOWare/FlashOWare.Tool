710b0dcfb49b146d843e9534e8e2e93afac3f2d7
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2023-12-17 11:45:45 +0100

    test: force exiting Process on timeout

--- src/tests/FlashOWare.Tool.Cli.Tests/Sdk/DotNet.New.cs
+++ src/tests/FlashOWare.Tool.Cli.Tests/Sdk/DotNet.New.cs
@@ -1,4 +1,4 @@
-using NuGet.Packaging;
+using FlashOWare.Tool.Cli.Tests.Diagnostics;
 using System.ComponentModel;
 using System.Diagnostics;
 
@@ -20,27 +20,9 @@ public partial class DotNet
     {
         string project = "WebApiAotProject";
 
-        ProcessStartInfo startInfo = new("dotnet");
-        startInfo.ArgumentList.AddRange(new[] { "new", "webapiaot", "--name", project, "--output", _directory.FullName, "--language", "C#", "--framework", "net8.0", "--exclude-launch-settings" });
-        startInfo.WorkingDirectory = _directory.FullName;
-        startInfo.CreateNoWindow = true;
-        startInfo.RedirectStandardOutput = true;
+        using Process process = StartProcess("dotnet", "new", "webapiaot", "--name", project, "--output", _directory.FullName, "--language", "C#", "--framework", "net8.0", "--exclude-launch-settings");
 
-        using var process = Process.Start(startInfo);
-        if (process is null)
-        {
-            throw new InvalidOperationException("No process resource is started.");
-        }
-
-        using CancellationTokenSource timeout = new(TimeSpan.FromSeconds(15));
-
-        await process.WaitForExitAsync(timeout.Token);
-
-        if (process.ExitCode != 0)
-        {
-            string output = await process.StandardOutput.ReadToEndAsync(timeout.Token);
-            throw new InvalidOperationException($"{nameof(Process.ExitCode)}: {process.ExitCode}{Environment.NewLine}{output}");
-        }
+        await process.WaitForSuccessfulExitAsync(TimeSpan.FromSeconds(15));
 
         return project;
     }
--- src/tests/FlashOWare.Tool.Cli.Tests/Sdk/DotNet.Restore.cs
+++ src/tests/FlashOWare.Tool.Cli.Tests/Sdk/DotNet.Restore.cs
@@ -1,4 +1,4 @@
-using NuGet.Packaging;
+using FlashOWare.Tool.Cli.Tests.Diagnostics;
 using System.Diagnostics;
 
 namespace FlashOWare.Tool.Cli.Tests.Sdk;
@@ -7,26 +7,8 @@ public partial class DotNet
 {
     internal async Task RestoreAsync(FileInfo project)
     {
-        ProcessStartInfo startInfo = new("dotnet");
-        startInfo.ArgumentList.AddRange(new[] { "restore", project.FullName });
-        startInfo.WorkingDirectory = _directory.FullName;
-        startInfo.CreateNoWindow = true;
-        startInfo.RedirectStandardOutput = true;
+        using Process process = StartProcess("dotnet", "restore", project.FullName);
 
-        using var process = Process.Start(startInfo);
-        if (process is null)
-        {
-            throw new InvalidOperationException("No process resource is started.");
-        }
-
-        using CancellationTokenSource timeout = new(TimeSpan.FromSeconds(15));
-
-        await process.WaitForExitAsync(timeout.Token);
-
-        if (process.ExitCode != 0)
-        {
-            string output = await process.StandardOutput.ReadToEndAsync(timeout.Token);
-            throw new InvalidOperationException($"{nameof(Process.ExitCode)}: {process.ExitCode}{Environment.NewLine}{output}");
-        }
+        await process.WaitForSuccessfulExitAsync(TimeSpan.FromSeconds(15));
     }
 }
