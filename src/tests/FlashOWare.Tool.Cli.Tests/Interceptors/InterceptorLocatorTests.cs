ae41d193fb2d34c8701e7a1fcfdd6374836168ac
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2023-12-18 03:04:07 +0100

    test(interceptors): skip tests that require .NET 8 on lower TFMs

--- src/tests/FlashOWare.Tool.Cli.Tests/Interceptors/InterceptorLocatorTests.cs
+++ src/tests/FlashOWare.Tool.Cli.Tests/Interceptors/InterceptorLocatorTests.cs
@@ -63,6 +63,11 @@ public class InterceptorLocatorTests : IntegrationTests
     [InlineData("--project")]
     public async Task List_WithGenerator_ListsInterceptors(string? option)
     {
+        if (MSBuild.IsLessThan(8, 0, 100))
+        {
+            return;
+        }
+
         //Arrange
         var project = Workspace.CreateProject()
             .AddDocument("""
@@ -89,31 +94,21 @@ public class InterceptorLocatorTests : IntegrationTests
             ? RunAsync("interceptor", "list")
             : RunAsync("interceptor", "list", option, project.File.FullName));
         //Assert
-        if (MSBuild.IsGreaterThanOrEqual(7, 0, 400))
-        {
-            Console.Verify($"""
-                Warning: Interceptors are an experimental feature, currently available in preview mode.
-                Project {project.Name} contains 2 interceptors with 2 interceptions.
+        Console.Verify($"""
+            Warning: Interceptors are an experimental feature, currently available in preview mode.
+            Project {project.Name} contains 2 interceptors with 2 interceptions.
 
-                2 interceptions in MyEnum.cs
-                - intercepted System.Enum.IsDefined<System.StringComparison>(System.StringComparison)
-                  at location MyEnum.cs:9:18
-                  Interceptor FlashOWare.Generated.EnumInterceptors.IsDefined0(System.StringComparison)
-                  at location FlashOWare.Generators{DirectorySeparator}FlashOWare.Generators.Enumerations.EnumInterceptorGenerator{DirectorySeparator}FlashOWare.Generated.EnumInterceptors.g.cs:23:24
-                - intercepted System.Enum.GetName<System.StringComparison>(System.StringComparison)
-                  at location MyEnum.cs:11:36
-                  Interceptor FlashOWare.Generated.EnumInterceptors.GetName0(System.StringComparison)
-                  at location FlashOWare.Generators{DirectorySeparator}FlashOWare.Generators.Enumerations.EnumInterceptorGenerator{DirectorySeparator}FlashOWare.Generated.EnumInterceptors.g.cs:34:27
-                """, null);
-            Result.Verify(ExitCodes.Success);
-        }
-        else
-        {
-            Console.Verify(null, $"""
-                The 'interceptors' experimental feature is not supported in .NET SDK {MSBuild.Instance.Version}.
-                """);
-            Result.Verify(ExitCodes.Success);
-        }
+            2 interceptions in MyEnum.cs
+            - intercepted System.Enum.IsDefined<System.StringComparison>(System.StringComparison)
+              at location MyEnum.cs:9:18
+              Interceptor FlashOWare.Generated.EnumInterceptors.IsDefined0(System.StringComparison)
+              at location FlashOWare.Generators{DirectorySeparator}FlashOWare.Generators.Enumerations.EnumInterceptorGenerator{DirectorySeparator}FlashOWare.Generated.EnumInterceptors.g.cs:23:24
+            - intercepted System.Enum.GetName<System.StringComparison>(System.StringComparison)
+              at location MyEnum.cs:11:36
+              Interceptor FlashOWare.Generated.EnumInterceptors.GetName0(System.StringComparison)
+              at location FlashOWare.Generators{DirectorySeparator}FlashOWare.Generators.Enumerations.EnumInterceptorGenerator{DirectorySeparator}FlashOWare.Generated.EnumInterceptors.g.cs:34:27
+            """, null);
+        Result.Verify(ExitCodes.Success);
     }
 
     [Theory]
@@ -121,37 +116,32 @@ public class InterceptorLocatorTests : IntegrationTests
     [InlineData("--group-by-interceptors")]
     public async Task List_GroupByInterceptors_ListsInterceptors(string option)
     {
+        if (MSBuild.IsLessThan(8, 0, 100))
+        {
+            return;
+        }
+
         //Arrange
         var project = await DotNet.NewAsync(DotNetNewTemplate.AspNetCoreWebApiNativeAot);
         //Act
         await RunAsync("interceptor", "list", option);
         //Assert
-        if (MSBuild.IsGreaterThanOrEqual(7, 0, 400))
-        {
-            Console.Verify($"""
-                Warning: Interceptors are an experimental feature, currently available in preview mode.
-                Project {project} contains 2 interceptors with 2 interceptions.
+        Console.Verify($"""
+            Warning: Interceptors are an experimental feature, currently available in preview mode.
+            Project {project} contains 2 interceptors with 2 interceptions.
 
-                2 Interceptors
-                - Interceptor: Microsoft.AspNetCore.Http.Generated.GeneratedRouteBuilderExtensionsCore.MapGet0(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder, string, System.Delegate)
-                  at location: Microsoft.AspNetCore.Http.RequestDelegateGenerator{DirectorySeparator}Microsoft.AspNetCore.Http.RequestDelegateGenerator.RequestDelegateGenerator{DirectorySeparator}GeneratedRouteBuilderExtensions.g.cs:62:45
-                  intercepts 1 method invocation:
-                  - Microsoft.AspNetCore.Routing.IEndpointRouteBuilder.MapGet(string, System.Delegate)
-                    - Program.cs:21:10
-                - Interceptor: Microsoft.AspNetCore.Http.Generated.GeneratedRouteBuilderExtensionsCore.MapGet1(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder, string, System.Delegate)
-                  at location: Microsoft.AspNetCore.Http.RequestDelegateGenerator{DirectorySeparator}Microsoft.AspNetCore.Http.RequestDelegateGenerator.RequestDelegateGenerator{DirectorySeparator}GeneratedRouteBuilderExtensions.g.cs:144:45
-                  intercepts 1 method invocation:
-                  - Microsoft.AspNetCore.Routing.IEndpointRouteBuilder.MapGet(string, System.Delegate)
-                    - Program.cs:22:10
-                """, null);
-            Result.Verify(ExitCodes.Success);
-        }
-        else
-        {
-            Console.Verify(null, $"""
-                The 'interceptors' experimental feature is not supported in .NET SDK {MSBuild.Instance.Version}.
-                """);
-            Result.Verify(ExitCodes.Success);
-        }
+            2 Interceptors
+            - Interceptor: Microsoft.AspNetCore.Http.Generated.GeneratedRouteBuilderExtensionsCore.MapGet0(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder, string, System.Delegate)
+              at location: Microsoft.AspNetCore.Http.RequestDelegateGenerator{DirectorySeparator}Microsoft.AspNetCore.Http.RequestDelegateGenerator.RequestDelegateGenerator{DirectorySeparator}GeneratedRouteBuilderExtensions.g.cs:62:45
+              intercepts 1 method invocation:
+              - Microsoft.AspNetCore.Routing.IEndpointRouteBuilder.MapGet(string, System.Delegate)
+                - Program.cs:21:10
+            - Interceptor: Microsoft.AspNetCore.Http.Generated.GeneratedRouteBuilderExtensionsCore.MapGet1(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder, string, System.Delegate)
+              at location: Microsoft.AspNetCore.Http.RequestDelegateGenerator{DirectorySeparator}Microsoft.AspNetCore.Http.RequestDelegateGenerator.RequestDelegateGenerator{DirectorySeparator}GeneratedRouteBuilderExtensions.g.cs:144:45
+              intercepts 1 method invocation:
+              - Microsoft.AspNetCore.Routing.IEndpointRouteBuilder.MapGet(string, System.Delegate)
+                - Program.cs:22:10
+            """, null);
+        Result.Verify(ExitCodes.Success);
     }
 }
